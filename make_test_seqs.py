0c8fa6d8ed5c8f2153a83ac4c2ac79e4e9c997ff
Yutaro <yhori@iam.u-tokyo.ac.jp>
2019-11-25 13:26:41 +0900

    simulated mismatch added

--- make_test_seqs.py
+++ make_test_seqs.py
@@ -1,8 +1,23 @@
 # This script makes an artificial .fastq file to submit to bwa mem.
 # Here I use mouse ribosomal rRNA consensus sequence.
 
-read_length = 1000
-with open('/home/yutaro/data/Mus_musculus_NCBI_GRCm38/Mus_musculus/NCBI/GRCm38/Sequence/AbundantSequences/musRibosomal.fa') as f:
+import random
+
+
+def simulating_read_errors(seq):
+    mutated_seq = ''
+    for n in seq:
+        if random.random() > 0.80:
+            mutated_seq += 'A'
+        else:
+            mutated_seq += n
+    return mutated_seq
+
+
+read_length = 4000
+filename_mus = '/home/yutaro/data/Mus_musculus_NCBI_GRCm38/Mus_musculus/NCBI/GRCm38/Sequence/AbundantSequences/musRibosomal.fa'
+filename_homo = '/home/yutaro/data/Homo_sapiens_NCBI_GRCh38/Homo_sapiens/NCBI/GRCh38/Sequence/AbundantSequences/humRibosomal.fa'
+with open(filename_homo) as f:
     f.readline()
     rDNA_seq = ''
     for line in f:
@@ -25,6 +40,12 @@ with open('/home/yutaro/data/cliveome/fastq_runid_0ee57c1a265c2f494821757929f1af
         if n > 200:
             break
 
-with open('test_data_mus.fastq', 'w') as fw:
+with open('test_data.fastq', 'w') as fw:
     for i in range(10):
         fw.write(headers[i] + '\n' + sample_seqs[i] + '\n+\n' + qualities[i] + '\n')
+
+with open('test_data_mutated.fastq', 'w') as fw:
+    for i in range(10):
+        fw.write(headers[i] + '\n' + simulating_read_errors(sample_seqs[i]) + '\n+\n' + qualities[i] + '\n')
+
+
